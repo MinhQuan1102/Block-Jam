c32d2c597b5cbc64d1407be33cdc03eeb614575b
mquan1102 <quandm@joybit.net>
2026-08-07 18:41:54 +0700

    .

--- Assets/Core/Modules/UI/Scripts/UIPage.cs
+++ Assets/Core/Modules/UI/Scripts/UIPage.cs
@@ -29,6 +29,39 @@ namespace Core
             graphicRaycaster = GetComponent<GraphicRaycaster>();
         }
 
+        public bool OnPrefabSaving()
+        {
+            Component[] cachedPageElements = GetComponentsInChildren(typeof(IUIPageElement));
+
+            if (registeredElements == null || registeredElements.Length != cachedPageElements.Length)
+            {
+                registeredElements = cachedPageElements;
+                return true;
+            }
+
+            for(int i = 0; i < registeredElements.Length; i++)
+            {
+                if (registeredElements[i] == null)
+                {
+                    registeredElements = cachedPageElements;
+
+                    return true;
+                }
+            }
+
+            for (int i = 0; i < cachedPageElements.Length; i++)
+            {
+                if(!ReferenceEquals(registeredElements[i], cachedPageElements[i]))
+                {
+                    registeredElements = cachedPageElements;
+
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
         public void OnSceneSaving()
         {
             throw new System.NotImplementedException();
